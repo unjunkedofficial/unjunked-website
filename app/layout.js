dedb15e8e253efc3f4e4a356ff5368aebd3236a9
unjunkedofficial <andy@ghalleyholdings.com>
2026-03-01 21:42:21 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,9 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <div className="site">{children}</div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
